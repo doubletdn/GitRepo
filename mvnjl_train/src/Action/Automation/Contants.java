9f4e5c639fe5f4456a2169b234736ecc1a8f04fb
doublet <hdkani@gmail.com>
2016-11-01 17:45:10 +0700

    cm

--- mvnjl_train/src/Action/Automation/Contants.java
+++ mvnjl_train/src/Action/Automation/Contants.java
@@ -5,4 +5,5 @@ public class Contants {
 	public static final String autURL = "https://https://doubletsample.joomla.com/administrator/index.php";
 	public static final String adminUser = "hdkani";
 	public static final String adminPass = "t123456";
+	public static final String configTest = "src/Resource/configTest.xml";
 }
